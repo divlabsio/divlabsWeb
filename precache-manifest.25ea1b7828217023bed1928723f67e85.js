d42240edbb2402a6a2bee93bb9712a2109d58e7d
quosss <lucas@MacBook-Pro-de-LUCAS.local>
2020-03-11 16:40:23 -0300

    Updates

---
+++ precache-manifest.25ea1b7828217023bed1928723f67e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3583f104289ec38560530a8734cd3c",
+    "url": "/divlabsWeb/index.html"
+  },
+  {
+    "revision": "5494bd546cdb58d48f51",
+    "url": "/divlabsWeb/static/css/main.a45e3636.chunk.css"
+  },
+  {
+    "revision": "b7343f7fb06fe5dfb9bd",
+    "url": "/divlabsWeb/static/js/2.e8b00746.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/divlabsWeb/static/js/2.e8b00746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5494bd546cdb58d48f51",
+    "url": "/divlabsWeb/static/js/main.8b5d2fed.chunk.js"
+  },
+  {
+    "revision": "de5cfba232840a3a654e",
+    "url": "/divlabsWeb/static/js/runtime-main.8f94de9d.js"
+  },
+  {
+    "revision": "5a12f9edf3e249c01632bf9f70d29bfe",
+    "url": "/divlabsWeb/static/media/logo.5a12f9ed.svg"
+  }
+]);
